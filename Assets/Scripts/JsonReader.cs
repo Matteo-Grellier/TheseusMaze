5e6f206d97189e36f87197cdad9d90383b2721f3
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2023-04-11 20:11:35 +0200

    :construction: working on json

---
+++ Assets/Scripts/JsonReader.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonReader : MonoBehaviour
+{
+    public TextAsset jsonText;
+
+    [System.Serializable]
+    public class Case {
+        public int caseID;
+        public string stateID;
+        public int roomID;
+    }
+
+    [System.Serializable]
+    public class Room {
+        public int roomID;
+        public int mazeID;
+        public bool leftDoor;
+        public bool upDoor;
+        public bool rightDoor;
+        public bool downDoor;
+        public bool isEndRoom;
+        public Case[] cases;
+    }
+
+    [System.Serializable]
+    public class Maze 
+    {
+        public int MazeID;
+        public Room[] rooms;
+    }
+
+    public Maze maze = new Maze();
+
+    // public class
+
+    private void Start() {
+        maze = JsonUtility.FromJson<Maze>(jsonText.text);
+    }
+}
